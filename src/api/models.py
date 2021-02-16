b3186d95d0ee6ee577eb8aa7ba7811a6d909add8
SleepNoMore <nninkovic.py@gmail.com>
2021-02-16 14:46:50 +0100

    Fixed a few bugs

--- src/api/models.py
+++ src/api/models.py
@@ -3,21 +3,21 @@ import os
 import uuid
 
 
-def recipe_image_file_path(instance, filename):
+def book_image_file_path(instance, filename):
     """
     Generate a file path and unique name for new recipe image
     """
     ext = filename.split(".")[-1]
     filename = f"{uuid.uuid4()}.{ext}"
 
-    return os.path.join("uploads/recipe/", filename)
+    return os.path.join("uploads/book/", filename)
 
 
 class Book(models.Model):
     author = models.CharField(max_length=50)
     title = models.CharField(max_length=50)
     description = models.TextField()
-    poster_image = models.ImageField(null=True, upload_to=recipe_image_file_path)
+    poster_image = models.ImageField(null=True, upload_to=book_image_file_path)
 
     def __str__(self):
         return self.title
